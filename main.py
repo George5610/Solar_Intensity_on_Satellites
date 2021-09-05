0c35f5f89304d7b44c3618a501cc9ddaa57fc1e6
George Johnson <georgejohnson5610@gmail.com>
2021-09-05 22:21:57 +0100

    optimisation, doesnt skip random data as much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from vpython import *
-import math
+from math import pi
 
 #
 # VARAIBLES
@@ -20,7 +20,7 @@ SHC = 900 # specific heat capacity of aluminium
 #FUNCTIONS
 #
 scene = canvas(title='Solar Thermal Radiation Model  ', x=0, y=0, width=1500, height=400)
-
+#cf = 1 # initial code freq
 running = True
 def Run(b):
     global running, remember_dt, dt
@@ -39,7 +39,7 @@ button(text="Pause", pos=scene.title_anchor, bind=Run)
 #CAMERA BUTTON
 def cam(b):
     global running
-    #running = not running
+    running = not running
     if running:
         b.text = "Sat"
         scene.camera.follow(Sun) # lock the camera to the sat
@@ -51,16 +51,28 @@ def cam(b):
 button(text="Sat", pos=scene.title_anchor, bind=cam)
 
 #ANIMATION SPEED
-scene.caption = "\nVary the rotation speed: \n\n"
+scene.caption = "\nChange Time Step: \n\n"
 
 def setspeed(s):
     wt.text = '{:1.2f}'.format(s.value)
 
-sl = slider(min=1, max=10000, value=1000, length=400, bind=setspeed, right=15)
+sl = slider(min=1, max=10000, value=1, length=400, bind=setspeed, right=15)
 
 wt = wtext(text='{:1.2f}'.format(sl.value))
 
 scene.append_to_caption(' Time Step \n')
+
+#Code frequency
+scene.append_to_caption("\nChange Code Frequency \n\n")
+
+def setfreq(f):
+    cft.text = '{:1.2f}'.format(f.value)
+
+cf = slider(min=1, max=500000, value=1, length=400, bind=setfreq, right=15)
+
+cft = wtext(text='{:1.2f}'.format(cf.value))
+
+scene.append_to_caption(' Frequency (Hz) \n')
 
 #
 #CREATING OBJECTS
@@ -74,10 +86,10 @@ sat.m = 100 # mass of the satellite (kg)
 sat.p = sat.m * vector(0,20000,0) # inital satellite vector
 
 #
-#LOOP AND CALCULATIONS
+#GRAPHS
 #
 t = 0 # set initial time
-
+counter = 0
 g1 = graph(scroll=True, width=1500, height=400, fast=False, xmin=0, xmax=100000, title="Intensity of Solar Radiation for distance", xtitle="Distance (km)", ytitle="Intensity (W/m^2)")
 g2 = graph(scroll=True, width=1500, height=400, fast=False, xmin=0, xmax=1e100, title="Intensity of Solar Radiation over time", xtitle="Time (s)", ytitle="Intensity (W/m^2)")
 g3 = graph(scroll=True, width=1500, height=400, fast=False, xmin=0, xmax=1e100, title="Temp of Sat", xtitle="Time (s)", ytitle="Temp (K)")
@@ -86,29 +98,38 @@ g1 = gcurve(graph=g1, color=color.green)
 g2 = gcurve(graph=g2, color=color.red)
 g3 = gcurve(graph=g3, color=color.blue)
 
+#
+#LOOP AND CALCULATIONS
+#
 while True: # set true for infinte or set to a max time
-    rate(200) # max frequency of the while loop (Hz)
+    rate(cf.value) # max frequency of the while loop (Hz)
     if running:
         #sat position calculations
         dt = sl.value # time step
         r = sat.pos - Sun.pos # distance from the sun
-        F = -G * Sun.m * sat.m * norm(r) / mag(r)**2 # force the satellite experiences due to gravity
+        magR = mag(r)
+        F = -G * Sun.m * sat.m * norm(r) / magR**2 # force the satellite experiences due to gravity
         sat.p = sat.p + F * dt # calculates new vector based on new time and force
         sat.pos = sat.pos + sat.p * dt / sat.m
 
-        #distance and intensity calcuations
-        magR = mag(r)
-        magRkm = magR / 1000
-        Intensity = HR / ((4*math.pi) * magR**2) # wattage per meter squared at the sats distance from the sun
-        print(Intensity, " Watts/m^2 at a distance of ", magRkm, " km")
+        #timers
+        counter += 1
         t = t + dt
-        g1.plot(magRkm, Intensity)
-        g2.plot(t, Intensity)
+
+        #distance and intensity calcuations
+        magRkm = magR / 1000
+        Intensity = HR / ((4*pi) * magR**2) # wattage per meter squared at the sats distance from the sun
 
         # thermal calculations
         PowerIn = Intensity * AF * A
         PowerOut = sigma * AF * A * Tsat**4 # energy emitted
         PowerNet = PowerIn - PowerOut
         Tsat = PowerNet / (sat.m * SHC) + Tsat
-        print(Tsat)
-        g3.plot(t, Tsat)
+
+        if not counter % 100: # plot sampling rate
+            #plots
+            g1.plot(magRkm, Intensity)
+            g2.plot(t, Intensity)
+            g3.plot(t, Tsat)
+            print(Tsat)
+            print(Intensity, " Watts/m^2 at a distance of ", magRkm, " km")
